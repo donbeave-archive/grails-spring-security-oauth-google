c41988f25ca06d5e1a4d5ae5b47fee4d30941f52
BeaVe <donbeave@gmail.com>
2013-11-02 00:56:06 +0800

    Fixed dependencies.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-grails.project.dependency.resolver = "maven"
+//grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     inherits("global") {
 
@@ -14,10 +14,11 @@ grails.project.dependency.resolution = {
         mavenCentral()
     }
     dependencies {
-        compile ':spring-security-oauth:2.0.2'
     }
 
     plugins {
+        compile ':spring-security-oauth:2.0.2'
+
         build(":release:3.0.1",
               ":rest-client-builder:1.0.3") {
             export = false
