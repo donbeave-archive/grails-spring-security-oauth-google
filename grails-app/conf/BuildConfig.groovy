7a6f24059477553a3c00bfaa535fcb5f6993e13d
BeaVe <donbeave@gmail.com>
2013-12-16 19:31:26 +0800

    Switch to Aether dependency manager.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,7 +2,7 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 
-//grails.project.dependency.resolver = "maven"
+grails.project.dependency.resolver = "maven"
 grails.project.dependency.resolution = {
     inherits("global") {
 
