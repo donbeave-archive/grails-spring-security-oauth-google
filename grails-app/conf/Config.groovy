ca73302d5dc8835d9dfbabfdde166fe47472053b
enr <enrico.comiti@gmail.com>
2014-05-15 22:36:47 +0200

    add configuration needed from tests

---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,34 @@
+// configuration for plugin testing - will not be included in the plugin zip
+
+log4j = {
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
+}
+
+// just for test, to avoid oauthService BeanCreationException "No oauth configuration found"
+environments {
+  test {
+    oauth {
+      providers {
+        google {
+            api = org.grails.plugin.springsecurity.oauth.GoogleApi20
+            key = 'oauth_google_key'
+            secret = 'oauth_google_secret'
+            successUri = '/oauth/google/success'
+            failureUri = '/oauth/google/failure'
+            callback = "${baseURL}/oauth/google/callback"
+            scope = 'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email'
+        }
+      }
+    }
+  }
+}
