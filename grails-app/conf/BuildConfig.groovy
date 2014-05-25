15964c7be31da1013da2212c4b2d8f0dbd5999d7
BeaVe <donbeave@gmail.com>
2014-05-25 18:26:26 +0800

    Upgraded to dependency spring-security-oauth to 2.1.0-RC4 version.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,4 +1,3 @@
-
 grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
@@ -20,7 +19,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        compile ':spring-security-oauth:2.1.0-RC3'
+        compile ':spring-security-oauth:2.1.0-RC4'
 
         build(':release:3.0.1', ':rest-client-builder:2.0.1') {
             export = false
