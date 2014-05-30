ac8eefa8a6a2481c636fecbaff643bf8fb8727fb
BeaVe <donbeave@gmail.com>
2014-05-31 06:02:29 +0800

    Fixed build on Grails 2.4.0.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -9,17 +9,22 @@ grails.project.dependency.resolution = {
     }
     log 'warn'
     repositories {
-        grailsCentral()
         mavenLocal()
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
         mavenCentral()
-        mavenRepo 'http://repo.spring.io/milestone'
-        mavenRepo 'https://raw.github.com/fernandezpablo85/scribe-java/mvn-repo'
     }
     dependencies {
     }
 
     plugins {
         compile ':spring-security-oauth:2.1.0-RC4'
+
+        // TODO we need this to compile in Grails 2.4...
+        compile ':hibernate:3.6.10.14', {
+            export = false
+        }
 
         build(':release:3.0.1', ':rest-client-builder:2.0.1') {
             export = false
